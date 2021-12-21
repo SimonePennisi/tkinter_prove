939fc3c93066dbaefa881d6387e03b3dd30c950d
wolfdead96 <59877082+wolfdead96@users.noreply.github.com>
2021-12-21 21:00:25 +0100

    prova repositori

--- provepython/provepython.py
+++ provepython/provepython.py
@@ -9,25 +9,25 @@ win.resizable(False,False)
 win.config(background="white")
 def first_command():
     text = "Hello World!"
-    text_out = tk.Label(win, text=text, fg="red", font=("Hevetica",20))
-    text_out.grid(row=0,column=1)
+    text_out = tk.Label(win, text=text, fg="red", font=("Hevetica",10))
+    text_out.grid(row=0,column=1,sticky="W")
 
-    first_button = tk.Button(text="Ok, sei strano", command=second_command)
-    first_button.grid(row=1,column=0)
+    second_button = tk.Button(text="Presenta", command=second_command)
+    second_button.grid(row=1,column=0, sticky="W")
 
 
 def second_command():
     text = "Mi chiamo ambrogio"
-    text_out = tk.Label(win, text=text, fg="green", font=("Hevetica",20))
-    text_out.grid(row=0,column=1,padx=100)
+    text_out = tk.Label(win, text=text, fg="green", font=("Hevetica",10))
+    text_out.grid(row=1,column=1,sticky="W")
 
 first_button = tk.Button(text="saluta", command=first_command)
-first_button.grid(row=0,column=0)
+first_button.grid(row=0,column=0, sticky="W")
 
 
 
 
-
+#gatto
 
 
 
